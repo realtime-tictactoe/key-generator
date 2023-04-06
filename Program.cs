29b035a3c7adbf76f13de1a41ef878eb43f339bc
Sameer Niaz <sameermohniaz@gmail.com>
2023-04-06 17:32:45 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+var key = AesKeyGen.GenerateKey();
+Console.WriteLine(key);
+
+public static class AesKeyGen
+{
+    public static string GenerateKey()
+    {
+        using (var aes = Aes.Create())
+        {
+            aes.GenerateKey();
+            return Convert.ToHexString(aes.Key);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TicTacToe: AESKeyGen
+Simple AES key generator
